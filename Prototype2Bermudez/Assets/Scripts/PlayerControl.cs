1fef0787492c038462afa460744a51ac399219d6
Daniel Bermudez <144481244+Bermudez-Daniel@users.noreply.github.com>
2024-03-14 11:40:37 -0700

    fixed stuff
    added code for farmer animals and projectile

--- Prototype2Bermudez/Assets/Scripts/PlayerControl.cs
+++ Prototype2Bermudez/Assets/Scripts/PlayerControl.cs
@@ -9,22 +9,31 @@ public class PlayerControl : MonoBehaviour
     public float xRange = 10;
     public float horizontalInput;
     public float speed = 10.0f;
-    
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
+      if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         
         
-        if (transform.position.x < xRange)
+        if (transform.position.x < -xRange)
 
         {
-          transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+          transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-            horizontalInput = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+           
+      
         }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
+    
 }
