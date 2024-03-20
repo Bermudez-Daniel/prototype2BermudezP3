9eaac0600f28db1a545755dfe5aa8b21ff3b2f07
Daniel Bermudez <144481244+Bermudez-Daniel@users.noreply.github.com>
2024-03-20 11:36:29 -0700

    Finnaly fixed spawnmanager
    didn't make much progress was playing yunity

--- Prototype2Bermudez/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2Bermudez/Assets/Scripts/DestroyOutOfBounds.cs
@@ -18,6 +18,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("GameOver");
             Destroy(gameObject);
         }
     }
--- Prototype2Bermudez/Assets/Scripts/SpawnManager.cs
+++ Prototype2Bermudez/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
-    
+    private float xSpawnRange = 20;
+    private float zSpawnPos = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+     void Start()
+    {
+       InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); 
+    }
     void Update()
     {
-     if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }      
+     
     }
-       void SpawnRandomAnimal()
-       { 
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20) spawnpos, 
-        animalPrefabs[animalIndex].transform.rotation);
-       }
+        void SpawnRandomAnimal()
+        {
+        Vector3 SpawnPos = new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, zSpawnPos);
 
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], SpawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+
+    
 }
 
